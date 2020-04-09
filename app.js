f349bd028ecc62600866dde090be92c4708d62a1
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-04-09 15:53:32 -0400

    basic rotating CD case w/ static lighting

---
+++ app.js
@@ -0,0 +1,70 @@
+// p5.disableFriendlyErrors = true;
+
+let container, canvas, easycam, img;
+
+function preload() {
+  img = loadImage("./cover.jpg");
+}
+
+function setup() {
+  container = document.querySelector("#p5-container");
+  setAttributes("antialias", true);
+
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.parent("p5-container");
+
+  // fix for EasyCam to work with p5 v0.9.0+
+  Dw.EasyCam.prototype.apply = function (n) {
+    var o = this.cam;
+    (n = n || o.renderer),
+      n &&
+        ((this.camEYE = this.getPosition(this.camEYE)),
+        (this.camLAT = this.getCenter(this.camLAT)),
+        (this.camRUP = this.getUpVector(this.camRUP)),
+        n._curCamera.camera(
+          this.camEYE[0],
+          this.camEYE[1],
+          this.camEYE[2],
+          this.camLAT[0],
+          this.camLAT[1],
+          this.camLAT[2],
+          this.camRUP[0],
+          this.camRUP[1],
+          this.camRUP[2]
+        ));
+  };
+  easycam = createEasyCam({ distance: 300 });
+
+  setupMouseBehavior();
+}
+
+function draw() {
+  background(0);
+
+  strokeWeight(0.25);
+  stroke(255);
+
+  ambientLight(255, 255, 255);
+
+  specularMaterial(250);
+
+  fill(255, 64, 0);
+
+  texture(img);
+  box(200, 175, 10);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
+  easycam.setViewport([0, 0, windowWidth, windowHeight]);
+}
+
+function setupMouseBehavior() {
+  document.oncontextmenu = function () {
+    return false;
+  };
+  document.onmousedown = function () {
+    return false;
+  };
+}
